ffa2d73748b452c84aa6652d73b89338407c2ca4
Himanshu Nagrath <33781906+hnagrath09@users.noreply.github.com>
2022-01-15 12:49:57 +0530

    added test cases for parser to support different function declarations

--- packages/core/src/parser.test.ts
+++ packages/core/src/parser.test.ts
@@ -1,6 +1,9 @@
 import parse from './parser'
 
 describe('parser', () => {
+  /**
+   * export default function foo() {}
+   */
   test('parses export default function statement correctly', () => {
     const input = `
     export default function sumNumbers(
@@ -35,6 +38,99 @@ describe('parser', () => {
             maxValue: 20,
             step: 1,
           },
+        },
+      ],
+    })
+  })
+  /**
+   * function foo() {}
+   *
+   * export default foo
+   */
+  test('parses function statement correctly, when declared and exported separately', () => {
+    const input = `
+    function sumNumbers(
+        /** @dozierParam First Number */ /** @maxValue 20 */ /** @minValue 5 */
+        firstNumber: number,
+        /** @dozierParam Second Number */ /** @maxValue 20 */ /** @minValue 5 */ /** @step 1 */
+        secondNumber: number,
+        ): number {
+            return firstNumber + secondNumber
+    }
+    export default sumNumbers
+    `
+    const result = parse(input)
+    expect(result).toEqual({
+      functionName: 'sumNumbers',
+      params: [
+        {
+          identifier: 'firstNumber',
+          label: 'First Number',
+          required: true,
+          type: 'number',
+          meta: {
+            minValue: 5,
+            maxValue: 20,
+          },
+        },
+        {
+          identifier: 'secondNumber',
+          label: 'Second Number',
+          required: true,
+          type: 'number',
+          meta: {
+            minValue: 5,
+            maxValue: 20,
+            step: 1,
+          },
+        },
+      ],
+    })
+  })
+  /**
+   * const foo = () => {}
+   *
+   * export default foo
+   */
+  test('parses function statement correctly when declared as arrow function and exported separately', () => {
+    const input = `
+    const deleteUser = async (userEmail: string) => {}
+    export default deleteUser
+    `
+    const result = parse(input)
+    expect(result).toEqual({
+      functionName: 'deleteUser',
+      params: [
+        {
+          identifier: 'userEmail',
+          label: 'User email',
+          required: true,
+          type: 'string',
+          meta: {},
+        },
+      ],
+    })
+  })
+  /**
+   * const foo = function() {}
+   *
+   * export default foo
+   */
+  test('parses function statement correctly when declared using a const and function expression and exported separately', () => {
+    const input = `
+    const deleteUser = async function(userEmail: string) {}
+    export default deleteUser
+    `
+    const result = parse(input)
+    expect(result).toEqual({
+      functionName: 'deleteUser',
+      params: [
+        {
+          identifier: 'userEmail',
+          label: 'User email',
+          required: true,
+          type: 'string',
+          meta: {},
         },
       ],
     })
